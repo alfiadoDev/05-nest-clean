c4a7112f2fb095cb881f895dbafc92880cc0dcad
Alfiado Constantino <alfiado.ngana@xit2s.com>
2025-06-23 20:30:47 +0200

    create auth decorator

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Post, UseGuards } from '@nestjs/common'
+import { CurrentUser } from 'src/auth/current-user-decorator'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import type { TokenPayloadSchema } from 'src/auth/jwt.strategy'
 import { PrismaService } from 'src/prisma/prisma.service'
 
 @Controller('questions')
@@ -8,7 +10,9 @@ export class CreateQuestionController {
   constructor(private prisma: PrismaService) { } // eslint-disable-line
 
   @Post()
-  async handle() {
+  async handle(@CurrentUser() user: TokenPayloadSchema) {
+    console.log(user)
+
     return 'ok'
   }
 }
