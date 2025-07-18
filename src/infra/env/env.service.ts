8b27f52342af0c59361728adc3e91b59378416d4
Alfiado Constantino <alfiado.ngana@xit2s.com>
2025-07-18 15:26:28 +0200

    create env module

---
+++ src/infra/env/env.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import type { Env } from './env'
+
+@Injectable()
+export class EnvService {
+  constructor(private configService: ConfigService<Env, true>) { } // eslint-disable-line
+
+  get<T extends keyof Env>(key: T) {
+    return this.configService.get<T>(key, { infer: true })
+  }
+}
