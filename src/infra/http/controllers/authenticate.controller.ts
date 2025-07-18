33ed9b6d5dbdb2c786033e0c8173cda541099285
Alfiado Constantino <alfiado.ngana@xit2s.com>
2025-07-18 15:04:20 +0200

    private routes by default

--- src/infra/http/controllers/authenticate.controller.ts
+++ src/infra/http/controllers/authenticate.controller.ts
@@ -10,6 +10,7 @@ import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation.pipe'
 import { z } from 'zod'
 import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student'
 import { WrongCredentialsError } from '@/domain/forum/application/use-cases/errors/wrong-credentials-error'
+import { Public } from '@/infra/auth/public.decorator'
 
 const authenticateBodySchema = z
   .object({
@@ -21,6 +22,7 @@ const authenticateBodySchema = z
 type AuthenticateBodyDTO = z.infer<typeof authenticateBodySchema>
 
 @Controller('sessions')
+@Public()
 export class AuthenticateController {
   constructor(private authenticateStudent: AuthenticateStudentUseCase) { } // eslint-disable-line
 
--- src/infra/http/controllers/create-account.controller.ts
+++ src/infra/http/controllers/create-account.controller.ts
@@ -10,6 +10,7 @@ import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation.pipe'
 import { z } from 'zod'
 import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student'
 import { StudentAlreadyExistsError } from '@/domain/forum/application/use-cases/errors/student-already-exists-error'
+import { Public } from '@/infra/auth/public.decorator'
 
 const createAccountBodySchema = z
   .object({
@@ -22,6 +23,7 @@ const createAccountBodySchema = z
 type CreateAccountBodyDTO = z.infer<typeof createAccountBodySchema>
 
 @Controller('accounts')
+@Public()
 export class CreateAccountController {
   constructor(private registerStudent: RegisterStudentUseCase) { } // eslint-disable-line 
 
